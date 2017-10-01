c3ab7c9ad59fd358e118d2592706b5dd6cbc3955
kensanni <kensanni@gmail.com>
2017-10-01 12:32:44 +0100

    Create user controller for signUp [#151543240]

---
+++ server/controllers/users.js
@@ -0,0 +1,22 @@
+import { User } from '../models';
+
+export default {
+  create(req, res) {
+    return User
+      .create({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        membership_type: req.body.membership_type,
+      })
+      .then(user => res.status(201).send({
+        success: true,
+        messge: 'Account created successfully',
+        username: user.username,
+        id: user.id,
+      }))
+      .catch(error => res.status(400).send(error));
+  },
+};

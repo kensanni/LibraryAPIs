e062bc77b0f53c49789551edfc8021dc6dbdbf78
kensanni <kensanni@gmail.com>
2017-10-09 09:34:40 +0100

    Update user model field [#151543240]

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,62 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  var User = sequelize.define('User', {
-    firstname: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstname: DataTypes.STRING,
+    lastname: DataTypes.STRING,
+    username: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter a username',
+      },
+      unique: {
+        args: true,
+        msg: 'Username already exists',
+      },
+      validate: {
+        len: {
+          args: [4, 10],
+          msg: 'Username should be between 4 to 10 characters',
+        },
+      },
+
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter a username',
+      },
+      unique: {
+        args: true,
+        msg: 'Email already exists',
+      },
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Please enter a valid email address',
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter a password',
+      },
+    },
+    membership_type: {
+      type: DataTypes.STRING,
+      defaultValue: 'Bronze',
+      validate: {
+        isIn: {
+          args: [['Bronze', 'Silver', 'Gold']],
+          msg: 'Membership should either be Bronze, Silver or Gold',
+        },
+      },
+    },
   });
+  User.associate = (models) => {
+
+  };
   return User;
 };

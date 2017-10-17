bb35c647f98881a5b38e9ff74ba974c99361db9f
kensanni <kensanni@gmail.com>
2017-10-17 03:22:37 +0100

    fix merge error

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,12 @@
+import controller from '../controllers';
 import bookController from '../controllers/books';
 
 export default (app) => {
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the Library API!',
+  }));
+
+  app.post('/api/users/signup', controller.users.create);
+  app.post('/api/users/signin', controller.users.signIn);
   app.post('/api/books', bookController.create);
 };
